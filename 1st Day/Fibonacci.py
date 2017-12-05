adbb39710c3523fffab4e47cd3c38d709cd97f31
unknown <sashka.lysenko@gmail.com>
2017-12-05 21:52:45 +0200

    Vargantfile and update of Fibonacci

--- 1st Day/Fibonacci.py
+++ 1st Day/Fibonacci.py
@@ -1,10 +1,17 @@
 def fibonacci(fib):
-    a = 0
-    b = 1
-    result = [0]
-    for i in range(0, fib - 1):
-        c = a + b
-        result.append(b)
-        a = b
-        b = c
-    print(result)
+    """returns array of Fibonacci"""
+
+    if fib < 0:
+        print("input should be a positive number")
+    elif fib == 1:
+        print([0, 1])
+    else:
+        a = 0
+        b = 1
+        result = [0]
+        for i in range(0, fib):
+            c = a + b
+            result.append(b)
+            a = b
+            b = c
+        print(result)
