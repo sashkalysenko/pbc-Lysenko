80b8e37edf6fc4a0f80fff8ece9295969cf02b27
unknown <sashka.lysenko@gmail.com>
2017-12-13 00:51:56 +0200

    added step to load config

--- pbc/sg/test_sel_grid.py
+++ pbc/sg/test_sel_grid.py
@@ -4,6 +4,7 @@ from pbc.sg.sg import Grid, StartGrid
 def test_sel_grid(ssh):
     grid = StartGrid(Grid(ssh))
     grid.download()
+    grid.download_node_config()
     grid.start_hub()
     grid.add_node()
     assert len(ssh.execute_command('pgrep java'))
